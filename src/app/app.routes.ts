04f8ca5ce37f1b882961b118fbe1b7d4a7730640
Cristobal Marti Mendez <cristobalmartimendez@gmail.com>
2025-02-08 18:49:54 +0100

    Inicio del proyecto

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { provideRouter } from '@angular/router';
 
-export const routes: Routes = [];
+import { CardListComponent } from './cards/card-list/card-list.component';
+import { CardDetailComponent } from './cards/card-detail/card-detail.component';
+import { DeckBuilderComponent } from './deck/deck-builder/deck-builder.component';
+import { DeckListComponent } from './deck/deck-list/deck-list.component';
+import { HomeComponent } from './home/home.component';
+
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'cards', component: CardListComponent },
+  { path: 'cards/:id', component: CardDetailComponent },
+  { path: 'decks', component: DeckListComponent },
+  { path: 'deck-builder', component: DeckBuilderComponent },
+  { path: '**', redirectTo: '', pathMatch: 'full' },
+];
