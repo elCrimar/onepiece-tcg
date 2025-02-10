a671e21a0501c0403971f2847eaacda9dff6a83f
Cristobal Marti Mendez <cristobalmartimendez@gmail.com>
2025-02-10 02:28:29 +0100

    Agregado nagvegacion entre cartas por teclado

--- src/app/cards/card-detail/card-detail.component.ts
+++ src/app/cards/card-detail/card-detail.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Component, Input, Output, EventEmitter, HostListener, ElementRef, AfterViewInit, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Card } from '../../models/card';
 
@@ -9,11 +9,18 @@ import { Card } from '../../models/card';
   templateUrl: './card-detail.component.html',
   styleUrls: ['./card-detail.component.scss']
 })
-export class CardDetailComponent {
+export class CardDetailComponent implements AfterViewInit {
   @Input() card?: Card;
   @Output() close = new EventEmitter<void>();
   @Output() previous = new EventEmitter<void>();
   @Output() next = new EventEmitter<void>();
+
+  @ViewChild('modalBackground', { static: true }) modalBackground!: ElementRef<HTMLDivElement>;
+
+  ngAfterViewInit(): void {
+    // Pone el foco en el fondo del modal para que se reciban los eventos de teclado.
+    this.modalBackground.nativeElement.focus();
+  }
 
   formatAbility(ability: string | undefined): string {
     if (!ability) return '';
@@ -27,5 +34,19 @@ export class CardDetailComponent {
 
   closeModal(): void {
     this.close.emit();
+  }
+
+  @HostListener('document:keydown', ['$event'])
+  handleKeyboardEvent(event: KeyboardEvent): void {
+    if (event.key === 'ArrowLeft') {
+      this.previous.emit();
+      event.preventDefault();
+    } else if (event.key === 'ArrowRight') {
+      this.next.emit();
+      event.preventDefault();
+    } else if (event.key === 'Escape') {
+      this.close.emit();
+      event.preventDefault();
+    }
   }
 }
