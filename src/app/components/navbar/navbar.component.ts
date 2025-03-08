5625b389e1a8dd0f937dfd27dd7b7ca657c0a9a4
Cristobal Marti Mendez <cristobalmartimendez@gmail.com>
2025-03-08 22:50:00 +0100

    Agregado iconos al navbar

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,15 +1,22 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faHome, faBook, faLayerGroup, faNewspaper } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, FontAwesomeModule],
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent {
+  faHome = faHome;
+  faBook = faBook;
+  faLayerGroup = faLayerGroup;
+  faNewspaper = faNewspaper;
+  
   isMobileMenuOpen = false;
   
   toggleMobileMenu(): void {
