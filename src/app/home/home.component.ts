b575997570ba3d5caa4991d3449b5d64ec834b6a
Cristobal Marti Mendez <cristobalmartimendez@gmail.com>
2025-03-28 18:43:11 +0100

    Agregado imagen en el menu principal e iconos a las tarjetas que ahora son botones tambien

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,14 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router'; 
 import { NavbarComponent } from '../components/navbar/navbar.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faNewspaper, faLayerGroup } from '@fortawesome/free-solid-svg-icons';
+import { faBook as faCards } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [NavbarComponent, RouterModule],
+  imports: [NavbarComponent, RouterModule, FontAwesomeModule],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
-
+  faCards = faCards;
+  faLayerGroup = faLayerGroup;
+  faNewspaper = faNewspaper;
 }
