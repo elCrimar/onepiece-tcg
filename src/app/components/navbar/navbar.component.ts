2cfaa0383a831ca0dcf077b9d19ee9053e6cf14e
Cristobal Marti Mendez <cristobalmartimendez@gmail.com>
2025-05-02 23:11:54 +0200

    Agregado boton cerrar sesion

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, computed, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { faHome, faBook, faLayerGroup, faNewspaper, faUser } from '@fortawesome/free-solid-svg-icons';
+import { faHome, faBook, faLayerGroup, faNewspaper, faUser, faXmark } from '@fortawesome/free-solid-svg-icons';
+import { AuthenticationService } from '../../services/authentication.service';
 
 @Component({
   selector: 'app-navbar',
@@ -17,6 +18,7 @@ export class NavbarComponent {
   faLayerGroup = faLayerGroup;
   faNewspaper = faNewspaper;
   faUser = faUser;
+  faXmark = faXmark;
   
   isMobileMenuOpen = false;
   
@@ -26,5 +28,19 @@ export class NavbarComponent {
   
   closeMobileMenu(): void {
     this.isMobileMenuOpen = false;
+  }
+
+  // Inyectar servicio de autenticación
+  private authService = inject(AuthenticationService);
+
+  // Usar las señales directamente desde el servicio
+  isAuthenticated = this.authService.isAuthenticated;
+
+  // Señal computada para obtener el nombre de usuario de forma segura
+  username = computed(() => this.authService.currentUser()?.username);
+
+  // Método para llamar al logout del servicio
+  logout(): void {
+    this.authService.logout();
   }
 }
